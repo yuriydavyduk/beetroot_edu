7fbd3e3e6a3817ec4052d640398038c93ef0b733
Yuriy Davyduk <yuriydavyduk@gmail.com>
2022-07-17 14:47:14 +0300

    homework lesson 3

--- homeworks/lesson_6_lists.py
+++ homeworks/lesson_6_lists.py
@@ -3,17 +3,19 @@ import random
 # Task 1
 random_numbers = list()
 
-while len(random_numbers) < 10:
+i = 0
+while i < 10:
     random_numbers.append(random.randint(1, 100))
+    i += 1
+
+max_number = 0
+i = 0
+while i < len(random_numbers):
+    if random_numbers[i] > max_number:
+        max_number = random_numbers[i]
+    i += 1
 
 print('List:', random_numbers)
-
-max_number = random_numbers.pop(0)
-while len(random_numbers) != 0:
-    number = random_numbers.pop(0)
-    if number > max_number:
-        max_number = number
-
 print('Max number:', max_number)
 
 # Task 2
@@ -22,17 +24,21 @@ list_1 = list()
 list_2 = list()
 new_list = list()
 
-while len(list_1) < 10:
+i = 0
+while i < 10:
     list_1.append(random.randint(1, 10))
     list_2.append(random.randint(1, 10))
+    i += 1
 
 print('List 1:', list_1)
 print('List 2:', list_2)
+i = 0
 
-while len(list_1) != 0:
-    element_of_list1 = list_1.pop(0)
+while i < len(list_1):
+    element_of_list1 = list_1[i]
     if element_of_list1 in list_2 and element_of_list1 not in new_list:
         new_list.append(element_of_list1)
+    i += 1
 
 print('Common numbers: ', new_list)
 
@@ -46,9 +52,11 @@ while i < 100:
     list_100.append(i)
     i += 1
 
-while len(list_100) != 0:
-    number = list_100.pop(0)
+i = 0
+
+while i < len(list_100):
+    number = list_100[i]
     if number % 7 == 0 and number % 5 != 0:
         list_7.append(number)
-
+    i += 1
 print(list_7)
