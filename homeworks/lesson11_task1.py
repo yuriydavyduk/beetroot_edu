c80a87e9ff4424bf21580e1e69738b45ae04b558
Yuriy Davyduk <yuriydavyduk@gmail.com>
2022-07-22 22:58:01 +0300

    homework lesson 3

---
+++ homeworks/lesson11_task1.py
@@ -0,0 +1,17 @@
+import os
+
+dir_name = os.path.dirname(__file__)
+
+def create_file():
+    with open(dir_name + '/myfile.txt', 'a') as f:
+        f.write('Hello file world!\n')
+        f.close()
+
+
+def print_file():
+    with open(dir_name + '/myfile.txt') as f:
+        print(f.read())
+        f.close()
+
+create_file()
+print_file()
