2871393a4b8d9aaa81fe8504b2b95ecfd82e2716
Yuriy Davyduk <yuriydavyduk@gmail.com>
2022-07-23 17:12:49 +0300

    homework lesson 3

--- homeworks/lesson11_task1.py
+++ homeworks/lesson11_task1.py
@@ -3,13 +3,13 @@ import os
 dir_name = os.path.dirname(__file__)
 
 def create_file():
-    with open(dir_name + '/myfile.txt', 'a') as f:
+    with open(os.path.join(dir_name,'myfile.txt'), 'a') as f:
         f.write('Hello file world!\n')
         f.close()
 
 
 def print_file():
-    with open(dir_name + '/myfile.txt') as f:
+    with open(os.path.join(dir_name,'myfile.txt')) as f:
         print(f.read())
         f.close()
 
