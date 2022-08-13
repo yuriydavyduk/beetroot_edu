d7691a67e1bd8fd5e0a59180d397612ee6fb7f14
Yuriy Davyduk <yuriydavyduk@gmail.com>
2022-08-13 22:30:49 +0300

    homework lesson 3

--- homeworks/lesson18_task3.py
+++ homeworks/lesson18_task3.py
@@ -1,15 +1,55 @@
 class TypeDecorators:
     @staticmethod
-    def to_int(star):
-        print('to_int')
+    def to_int(func):
+        def wrap(param):
+            try:
+                return int(param)
+            except TypeError:
+                return param
 
-    def to_str(self):
-        print('to_str')
+        return wrap
+
+    @staticmethod
+    def to_str(func):
+        def wrap(param):
+            try:
+                return str(param)
+            except TypeError:
+                return param
+
+        return wrap
+
+    @staticmethod
+    def to_bool(func):
+        def wrap(param):
+            try:
+                return True if param == 'True' else False
+            except TypeError:
+                return param
+
+        return wrap
+
+    @staticmethod
+    def to_float(func):
+        def wrap(param):
+            try:
+                return float(param)
+            except TypeError:
+                return param
+
+        return wrap
 
 
 @TypeDecorators.to_int
-def do_nothing(star):
+def do_nothing(string: str):
+    return string
 
-    return star
 
-print(do_nothing('123'))
+@TypeDecorators.to_bool
+def do_something(string: str):
+    return string
+
+
+assert do_nothing('25') == 25
+
+assert do_something('True') is True
